d53c44ff60e6d58160a7ff53bf9f6801ba3bea44
mxvc <410518072@qq.com>
2025-11-10 22:17:39 +0800

    init

---
+++ src/main/java/io/github/jian41/springadminframework/SpringAdminFrameworkApplication.java
@@ -0,0 +1,13 @@
+package io.github.jian41.springadminframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdminFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdminFrameworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/jian41/springadminframework/SpringAdminFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jian41.springadminframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdminFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
