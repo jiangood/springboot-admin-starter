dbf193341de3c71635967911f5e099088c055345
mxvc <410518072@qq.com>
2025-11-26 19:53:00 +0800

    流程-服务组件 异步选择ok

--- web/src/pages/flowable/design/provider/magic/parts/ServiceTaskProps.js
+++ web/src/pages/flowable/design/provider/magic/parts/ServiceTaskProps.js
@@ -1,6 +1,7 @@
-import { TextFieldEntry, isTextFieldEntryEdited } from '@bpmn-io/properties-panel';
+import {TextFieldEntry, isTextFieldEntryEdited, SelectEntry} from '@bpmn-io/properties-panel';
 import { useService } from 'bpmn-js-properties-panel';
-
+import { useEffect, useState } from '@bpmn-io/properties-panel/preact/hooks';
+import {HttpUtils} from "../../../../../../framework";
 export  function ServiceTaskProps () {
 
   return [
@@ -29,14 +30,25 @@ function ServiceTaskComponent(props) {
     });
   };
 
-  return TextFieldEntry({
+  const [ options, setOptions ] = useState([]);
+
+  useEffect(async () => {
+    const rs = await HttpUtils.get('admin/flowable/model/javaDelegateOptions')
+    setOptions(rs)
+  }, [ setOptions ]);
+
+  return SelectEntry({
     element,
     id: id,
     label: 'Java服务类表达式',
     description: '实现了JavaDelegate的Bean名称表达式， 如 ${demoDelegate}',
     getValue,
     setValue,
-    debounce
+    debounce,
+
+    getOptions: () => {
+      return options
+    }
   })
 
 }
