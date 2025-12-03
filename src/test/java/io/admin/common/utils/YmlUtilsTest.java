c5468e985a491fc48e9577f3bec4bfab3b161e32
jiangtao <jiangtao.cctv@qq.com>
2025-12-03 15:08:25 +0800

    测试

--- src/test/java/io/admin/common/utils/YmlUtilsTest.java
+++ src/test/java/io/admin/common/utils/YmlUtilsTest.java
@@ -1,17 +1,20 @@
 package io.admin.common.utils;
 
+import cn.hutool.core.io.resource.ResourceUtil;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.util.ResourceUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 class YmlUtilsTest {
 
     @Test
     void parseYml() throws IOException {
-        AppConfig config = YmlUtils.parseYml("classpath:config.yml", AppConfig.class);
+        InputStream is = ResourceUtil.getStream("classpath:config.yml");
+        AppConfig config = YmlUtils.parseYml(is, AppConfig.class, null);
 
         System.out.println(config.applicationName); // Output: My Awesome App
         System.out.println(config.maxThreads);     // Output: 50
@@ -21,8 +24,8 @@ class YmlUtilsTest {
 
     @Test
     void parseYmlPrefix() throws IOException {
-        File file = ResourceUtils.getFile("classpath:config-prefix.yml");
-        AppConfig config = YmlUtils.parseYml(file, AppConfig.class,"app");
+        InputStream is = ResourceUtil.getStream("classpath:config-prefix.yml");
+        AppConfig config = YmlUtils.parseYml(is, AppConfig.class,"app");
 
         System.out.println(config.applicationName); // Output: My Awesome App
         System.out.println(config.maxThreads);     // Output: 50
