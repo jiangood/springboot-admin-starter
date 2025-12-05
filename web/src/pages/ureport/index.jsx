30ef90aa6845892389e436484bc6b4bbf6eb1e4a
mxvc <410518072@qq.com>
2025-12-05 21:04:29 +0800

    优化

--- web/src/pages/ureport/index.jsx
+++ web/src/pages/ureport/index.jsx
@@ -1,36 +1,16 @@
 import React from 'react'
+import {Page} from "../../framework";
 import {Button} from "antd";
-import {ButtonList, HttpUtils, Page, ProTable} from "../../framework";
 
 export default class extends React.Component {
 
-    state = {
-        status: {}
-    }
 
 
     render() {
         return <Page padding>
 
-            <ProTable
-                toolBarRender={() => <Button type='primary'
-                                             target='_blank'
-                                             href={"/ureport/designer"}>打开设计器</Button>}
 
-                request={(params) => HttpUtils.get('admin/ureport/page', params)}
-                columns={[
-                    {title: "文件名称", dataIndex: 'name'},
-                    {title: "存储器", dataIndex: 'providerName'},
-                    {title: "存储器前缀", dataIndex: 'providerPrefix'},
-                    {title: "更新时间", dataIndex: 'updateDate'},
-                    {title: "-", dataIndex: 'option',
-                        render(_,record){
-                            return <ButtonList >
-                                <Button size='small' perm='ureport:view' href={record.previewUrl} target='_blank'>预览</Button>
-                                <Button size='small' perm='ureport:design' href={record.designerUrl} target='_blank'>设计</Button>
-                            </ButtonList>
-                        }},
-                ]}></ProTable>
+            <Button type='primary' href='/ureport/designer'>打开设计器</Button>
 
 
         </Page>
