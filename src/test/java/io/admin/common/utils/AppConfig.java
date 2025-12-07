e459d2bde7aa1a0ae9efe5131b6edc448c755fdf
mxvc <410518072@qq.com>
2025-12-07 16:03:14 +0800

    ai

--- src/test/java/io/admin/common/utils/AppConfig.java
+++
@@ -1,6 +0,0 @@
-package io.admin.common.utils;
-
-public class AppConfig {
-    public String applicationName;
-    public int maxThreads; // Corresponds to max-threads in YAML
-}
--- src/test/java/io/admin/framework/data/specification/UserDao.java
+++
@@ -1,8 +0,0 @@
-package io.admin.framework.data.specification;
-
-import io.admin.framework.data.repository.BaseDao;
-import org.springframework.stereotype.Component;
-
-@Component
-public class UserDao extends BaseDao<User> {
-}
