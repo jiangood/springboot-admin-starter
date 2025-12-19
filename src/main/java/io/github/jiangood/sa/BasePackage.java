931771cde1b573d6fb6a3139ae32f8bd563b5ac3
jiangtao <jiangtao.cctv@qq.com>
2025-12-19 15:27:16 +0800

    自动配置

--- src/main/java/io/github/jiangood/sa/BasePackage.java
+++ src/main/java/io/github/jiangood/sa/BasePackage.java
@@ -1,11 +1,15 @@
 package io.github.jiangood.sa;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @ComponentScan(basePackages = {"io.github.jiangood.sa.framework", "io.github.jiangood.sa.modules"})
 @EntityScan(basePackages = "io.github.jiangood.sa.modules")
+@EnableCaching
+@EnableScheduling
 public class BasePackage {
 }
