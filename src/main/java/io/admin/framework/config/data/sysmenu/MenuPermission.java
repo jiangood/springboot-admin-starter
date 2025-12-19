26c2362cc1ded194a07e18bbad941f45f3809371
jiangtao <jiangtao.cctv@qq.com>
2025-12-19 11:27:55 +0800

    修改工具类为tools

--- src/main/java/io/admin/framework/config/data/sysmenu/MenuPermission.java
+++ src/main/java/io/admin/framework/config/data/sysmenu/MenuPermission.java
@@ -1,7 +1,11 @@
 package io.admin.framework.config.data.sysmenu;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class MenuPermission {
     String name;
