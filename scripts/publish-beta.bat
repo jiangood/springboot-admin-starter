557178e57bb6ceca9664a4b26583404241895903
jiangood <410518072@qq.com>
2025-12-24 11:37:38 +0800

    修改message为holder模式

--- scripts/publish-beta.bat
+++ scripts/publish-beta.bat
@@ -1,10 +1,9 @@
-cd ../web
+cd ..
+call mvnw clean deploy -P rdc
 
-
+cd web
 call pnpm version prerelease --preid=beta
-
 call pnpm build
-
 call pnpm publish -r --access public --no-git-checks --tag beta --registry https://packages.aliyun.com/62d39be70065edd3d51c1984/npm/npm-registry/
 
 
